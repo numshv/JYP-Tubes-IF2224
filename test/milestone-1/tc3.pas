3be86cb46b9088cd005c7fb122ba11c8768db8a6
numshv <noumisyifa@gmail.com>
2025-10-17 10:41:55 +0700

    test: add test case variations

---
+++ test/milestone-1/tc3.pas
@@ -0,0 +1,19 @@
+program loopdeloop;
+var
+    numbers: array[1 .. 10] of integer;
+    i, total: integer;
+begin
+    total := 0;
+    for i := 1 to 10 do
+    begin
+        numbers[i] := i * 2;
+        total := total + numbers[i];
+    end;
+    
+    i := 10;
+    while i >= 1 do
+    begin
+        writeln('Element ', i, ' = ', numbers[i]);
+        i := i - 1;
+    end;
+end.
---
+++ test/milestone-1/tc5.pas
@@ -0,0 +1,30 @@
+program EdgeCaseNumbersAndStrings;
+var
+    zero: integer;
+    negative: integer;
+    huge: integer;
+    pi: real;
+    epsilon: real;
+    empty: char;
+    quote: char;
+begin
+    zero := 0;
+    negative := -999;
+    huge := 2147483647;
+    pi := 3.14159265359;
+    epsilon := 0.00001;
+    
+    empty := ' ';
+    quote := '''';
+    
+    if zero = 0 then
+        writeln('Zero works');
+    
+    if negative < 0 then
+        writeln('Negative: ', negative);
+    
+    writeln('Pi = ', pi);
+    writeln('String with spaces:  multiple  spaces');
+    writeln('Special: ', quote);
+    writeln('Numbers:', 1, 22, 333, 4444);
+end.
