06635c7bd31a97afbc94becc0d9447fa9288a231
numshv <noumisyifa@gmail.com>
2025-11-06 21:48:59 +0700

    test: update tc to idn

---
+++ test/milestone-1/tc7_id.pas
@@ -0,0 +1,8 @@
+program loopdeloop;
+variabel
+    numbers: larik[1 .. 10] dari integer;
+    i, total: integer;
+mulai
+    numbers[i] := 42; 
+    writeln('Element ', i, ' = ', numbers[i]);
+selesai.
---
+++ test/milestone-1/tc_9.pas
@@ -0,0 +1,17 @@
+program EXAMPLE1;
+variabel
+    x, y, sum: real;
+mulai
+    x := 5;
+    y := 10;
+    sum := x + y;
+
+    writeln('Sum = ', sum);
+
+    jika sum > 10 dan sum < 20 maka
+        writeln('Sum > 10')
+    selain-itu
+        writeln('Sum <= 10');
+
+    writeln('Done!');
+selesai.
