6bfb3aa9945c44c5b712352beb9e4d839221c7c4
Diyah Susan <diyahnugrahani04@gmail.com>
2025-11-16 11:50:41 +0700

    test : add tc

---
+++ test/milestone-2/tc4.pas
@@ -0,0 +1,14 @@
+program test_boolean;
+variabel
+    a, b: boolean;
+mulai
+    a := benar;
+    b := salah;
+
+    jika a dan tidak b maka
+        writeln('Case 1');
+
+    jika a atau b maka
+        writeln('Case 2');
+
+selesai.
---
+++ test/milestone-2/tc5.pas
@@ -0,0 +1,8 @@
+program chain;
+variabel
+    a, b, c : integer;
+mulai
+    a := 5;
+    b := a + 3;
+    c := b * a + (b - a);
+selesai.
