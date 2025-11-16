2814ba32ff5fe4a520f85496ad30b276b559e921
numshv <noumisyifa@gmail.com>
2025-11-16 11:18:40 +0700

    chore: move & add tc

---
+++ test/milestone-2/err1.pas
@@ -0,0 +1,14 @@
+program DoubleDeclaration;
+variabel
+    x, y: integer;
+    z: real;
+konstanta
+    MAX := 100;
+variabel
+    a, b: integer;
+mulai
+    x := 10;
+    y := 20;
+    z := x + y;
+    a := MAX;
+selesai.
