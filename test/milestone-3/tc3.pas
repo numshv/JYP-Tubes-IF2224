a830fa0932fe2c56f3fc59d70996d40132b959b2
Diyah Susan <diyahnugrahani04@gmail.com>
2025-11-30 08:22:46 +0700

    test : add tc

--- test/milestone-3/tc3.pas
+++ test/milestone-3/tc3.pas
@@ -1,18 +1,17 @@
-program ShadowTest;
+program UjiFaktorial;
 variabel
-    data: integer; 
+    hasil, n : integer;
 
-prosedur proses(n: integer);
-variabel
-    listLokal: larik[1 .. 5] dari integer; 
-    i: integer;
+fungsi faktorial(k : integer) : integer;
 mulai
-    listLokal[1] := n * 10;
-    
-    data := listLokal[1]; 
+    jika (k <= 1) maka
+        faktorial := 1;
+    selain-itu
+        faktorial := k * faktorial(k - 1);
 selesai;
 
 mulai
-    data := 999;
-    proses(5);
+    n := 5;
+    hasil := faktorial(n);
+    writeln('Faktorial 5 = ', hasil);
 selesai.
